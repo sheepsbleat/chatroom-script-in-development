2ec4133d70d7c8fcec8592a4b243579e8b4ceb26
sheepsbleat <50273094+sheepsbleat@users.noreply.github.com>
2019-05-06 13:48:10 +0100

    Create media.js
    work in progress and may not work
    optional

---
+++ media.js
@@ -0,0 +1,11 @@
+//in progress and may not work
+create.import(customCheck)
+import @media
+import game
+import window
+import update
+(input)text {
+img = desktop.import (type= 'button' onclick)resolute[func.file.export]
+}
+let media= media + 50
+log media
